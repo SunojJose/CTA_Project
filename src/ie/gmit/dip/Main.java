9278d49dfdbe1f35ba4e3e900a804d9a3999a766
sJose <73960381+SunojJose@users.noreply.github.com>
2020-12-10 17:23:41 +0000

    Update Main.java

--- src/ie/gmit/dip/Main.java
+++ src/ie/gmit/dip/Main.java
@@ -21,46 +21,46 @@ public class Main {
 		
 		int[] sizes = {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000}; 
 
-        int numberOfRuns = 10;
-        
-        Sort sort = new BubbleSort();
-        
-        Util util = new Util();
+		int numberOfRuns = 10;
 
-        System.out.print("Algorithm|Size:\t");
-        
-        util.printArray(sizes);
+		Sort sort = new BubbleSort();
 
-        System.out.println();
+		Util util = new Util();
 
-        System.out.print("Bubble Sort\t");
-        
-        print(sizes, numberOfRuns, sort);
-        
-        sort = new SelectionSort();
-        
-        System.out.print("Selection Sort\t");
-        
-        print(sizes, numberOfRuns, sort);
-        
-        sort = new MergeSort();
-        
-        System.out.print("Merge Sort\t");
-        
-        print(sizes, numberOfRuns, sort);
+		System.out.print("Algorithm|Size:\t");
 
-        sort = new QuickSort();
-        
-        System.out.print("Quick Sort\t");
-        
-        print(sizes, numberOfRuns, sort);
-        
-        sort = new CountingSort();
-        
-        System.out.print("Counting Sort\t");
-        
-        print(sizes, numberOfRuns, sort);
-        
+		util.printArray(sizes);
+
+		System.out.println();
+
+		System.out.print("Bubble Sort\t");
+
+		print(sizes, numberOfRuns, sort);
+
+		sort = new SelectionSort();
+
+		System.out.print("Selection Sort\t");
+
+		print(sizes, numberOfRuns, sort);
+
+		sort = new MergeSort();
+
+		System.out.print("Merge Sort\t");
+
+		print(sizes, numberOfRuns, sort);
+
+		sort = new QuickSort();
+
+		System.out.print("Quick Sort\t");
+
+		print(sizes, numberOfRuns, sort);
+
+		sort = new CountingSort();
+
+		System.out.print("Counting Sort\t");
+
+		print(sizes, numberOfRuns, sort);
+
 	}
 
 	/**
